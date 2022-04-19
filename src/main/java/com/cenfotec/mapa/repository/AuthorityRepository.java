9f6b25637ea60adde261c25863cda98080bd9795
arsereg <arsereg@gmail.com>
2022-04-18 19:49:32 -0600

    Initial version of ProyectoEstructurasDos generated by generator-jhipster@7.7.0

---
+++ src/main/java/com/cenfotec/mapa/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.cenfotec.mapa.repository;
+
+import com.cenfotec.mapa.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
---
+++ src/main/java/com/cenfotec/mapa/web/rest/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Spring MVC REST controllers.
+ */
+package com.cenfotec.mapa.web.rest;
